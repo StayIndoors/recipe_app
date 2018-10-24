9cf5feccbfc60dd737f2e004706f6d1819ea517e
Matthew Burnside <matthewsburnside@gmail.com>
2018-10-24 14:34:29 -0700

    Now retrieves parameters to determine if in edit mode.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -14,6 +14,7 @@ export class RecipeDetailComponent implements OnInit {
   id: number;
 
   constructor(private recipeService: RecipeService,
+              private router: Router,
               private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -29,4 +30,11 @@ export class RecipeDetailComponent implements OnInit {
   onAddToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], {relativeTo: this.route});
+    // This is a more complex path for example
+    // this.router.navigate(['../', this.id, 'edit'], {relativeTo: this.route});
+  }
 }
+
