e626d97e243c9054639fc13989ae13771c014234
Matthew Burnside <matthewsburnside@gmail.com>
2018-11-15 19:44:53 -0800

    Configure the delete and cancel buttons in recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -57,4 +57,9 @@ export class RecipeService {
     updateRecipes() {
         this.recipesChanged.next(this.recipes.slice());
     }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index, 1);
+        this.updateRecipes();
+    }
 }
